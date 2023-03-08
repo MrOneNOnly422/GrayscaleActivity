7a4aeb41cd2ea192cbac18a7e179e3db3f6db19a
QQ107 <QQ107@CSLAB239>
2023-03-08 09:34:57 +0800

    Add project files.

---
+++ GrayscaleActivity/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrayscaleActivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap pic = new Bitmap("C:\\Users\\QQ107\\Pictures\\Banner.jpg");
+
+            pictureBox1.Image = Image.FromFile("C:\\Users\\QQ107\\Pictures\\Banner.jpg");
+
+            for (int y = 0; y < pic.Height; y++)
+            {
+                for (int x = 0; x < pic.Width; x++)
+                {
+                    Color c = pic.GetPixel(x, y);
+
+                    int r = c.R;
+                    int g = c.G;
+                    int b = c.B;
+                    int avg = (r + g + b) / 3;
+                    pic.SetPixel(x, y, Color.FromArgb(avg, avg, avg));
+                }
+            }
+
+            pictureBox2.Image = pic;
+
+            pic.Save("C:\\Users\\QQ107\\Pictures\\Banner.jpg");
+            }
+        }
+    }
+
